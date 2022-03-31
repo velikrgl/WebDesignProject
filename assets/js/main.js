569b83cf17e77805930780fbbb4535e74c2293f0
emrhnrl <emrhnrl@hotmail.com>
2022-03-31 13:54:15 +0300

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Moderna - v4.8.0
-* Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
