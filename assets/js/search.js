fe7ce0fe17d556ce1d7512922b696e58231546a2
velikrgl <velikrgl@yahoo.com>
2022-06-02 11:35:37 +0300

    gallery page fixed

---
+++ assets/js/search.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var searchTerm;
+    $("#btnSearch").click(function() {
+        searchTerm = "sports";
+           
+            var url = "http://api.flickr.com/services/feeds/" +
+                "photos_public.gne?format=json&jsoncallback=?" +
+                "&tags=" + searchTerm + "&tagmode=all";
+
+            $.getJSON(url, function(data){
+                var html = "";
+                $.each(data.items, function(i, item){
+                    // html += "<h2>" + item.title + "</h2>";	
+                    html += "<img src=" + item.media.m + ">";
+                    // html += "<p><b>Tags: </b>" + item.tags + "</p>";
+                });
+                $("#photos").html(html);
+            });          
+        
+    });
+});
